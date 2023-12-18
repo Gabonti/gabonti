ad4d23bdf5b2162addc36b12ec8525023e1c8868
Gabonti <gabdilkarim05@mail.ru>
2023-12-19 01:13:46 +0600

    ninth commit

--- js/script.js
+++ js/script.js
@@ -22,8 +22,8 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         dots: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
         responsive: [
           {
             breakpoint: 991,
